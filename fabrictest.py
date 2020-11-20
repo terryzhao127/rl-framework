85b37a388f81ea078f16d477a1e4c753c364bc5c
AltmanD <luyu.dong@foxmail.com>
2020-11-20 09:06:02 +0000

    fabrictest

--- fabrictest.py
+++ fabrictest.py
@@ -2,9 +2,13 @@ from fabric import Connection
 c = Connection(
     host="localhost",
     connect_kwargs={
-        "password": "dongdong888",
+        "password": "123456",
     },
 )
 result = c.run('uname -s', hide=True)
 msg = "Ran {0.command!r} on {0.connection.host}, got stdout:\n{0.stdout}"
+print(msg.format(result))
+result = c.run("conda activate framework", hide=True)
+result = c.run('pytest -s /workplace/rl-framework/leatest.py', hide=True)
+msg = "got stdout:\n{0.stdout}"
 print(msg.format(result))
