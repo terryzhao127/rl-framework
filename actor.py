b79edc5e84ec25ca0eb19bcc326cbdb17630287f
Guikarist <guikarist@gmail.com>
2020-12-12 14:20:10 +0800

    Asynchronous training pattern basically established

--- actor.py
+++ actor.py
@@ -1,14 +1,16 @@
 import os
 import pickle
 from argparse import ArgumentParser
+from itertools import count
 from multiprocessing import Process
+from pathlib import Path
 
 import numpy as np
 import zmq
 
-from utils import logger
 from common import init_components
 from core import DataCollection
+from utils import logger
 from utils.cmdline import parse_cmdline_kwargs
 
 parser = ArgumentParser()
@@ -16,11 +18,13 @@ parser.add_argument('--alg', type=str, help='The RL algorithm', required=True)
 parser.add_argument('--env', type=str, help='The game environment', required=True)
 parser.add_argument('--num_steps', type=float, help='The number of training steps', required=True)
 parser.add_argument('--ip', type=str, help='IP address of learner server', required=True)
-parser.add_argument('--port', type=int, default=5000, help='Learner server port')
+parser.add_argument('--data_port', type=int, default=5000, help='Learner server port to send training data')
+parser.add_argument('--param_port', type=int, default=5001, help='Learner server port to subscribe model parameters')
 parser.add_argument('--num_replicas', type=int, default=1, help='The number of actors')
 parser.add_argument('--model', type=str, default=None, help='Training model')
 parser.add_argument('--n_step', type=int, default=1, help='The number of sending data')
 parser.add_argument('--log_path', type=str, default=None, help='Directory to save logging data')
+parser.add_argument('--ckpt_path', type=str, default='./ckpt/', help='Directory to save model parameters')
 
 
 def run_one_agent(index, args, unknown_args):
@@ -35,7 +39,7 @@ def run_one_agent(index, args, unknown_args):
     # Connect to learner
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
-    socket.connect(f'tcp://{args.ip}:{args.port}')
+    socket.connect(f'tcp://{args.ip}:{args.data_port}')
 
     env, agent = init_components(args, unknown_args)
 
@@ -49,6 +53,8 @@ def run_one_agent(index, args, unknown_args):
 
     data_collection = DataCollection(args.n_step)
 
+    model_id = -1
+
     state = env.reset()
     for step in range(args.num_steps):
         # Do some updates
@@ -61,11 +67,12 @@ def run_one_agent(index, args, unknown_args):
         data = data_collection.push(state, action, action_prob, reward, next_state, done)
         if data is not None:
             socket.send(data)
+            socket.recv()
 
-            # Update weights
-            weights = socket.recv()
-            if len(weights):
-                agent.set_weights(pickle.loads(weights))
+        # Update weights
+        new_weights = find_new_weights(model_id, args.ckpt_path)
+        if new_weights is not None:
+            agent.set_weights(new_weights)
 
         state = next_state
         episode_rewards[-1] += reward
@@ -83,11 +90,51 @@ def run_one_agent(index, args, unknown_args):
             episode_rewards.append(0.0)
 
 
+def run_weights_subscriber(args):
+    """Subscribe weights from Learner and save them locally"""
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect(f'tcp://{args.ip}:{args.param_port}')
+    socket.setsockopt_string(zmq.SUBSCRIBE, '')  # Subscribe everything
+
+    for model_id in count(1):
+        weights = socket.recv()
+        with open(args.ckpt_path / f'{model_id}.{args.alg}.{args.env}.ckpt', 'wb') as f:
+            pickle.dump(weights, f)
+
+
+def find_new_weights(current_model_id: int, ckpt_path: Path):
+    try:
+        latest_file = max(ckpt_path.glob('*'), key=lambda p: p.stat().st_ctime)
+    except ValueError:
+        # No checkpoint file
+        return None
+
+    if int(latest_file.name.split('.')[0]) > current_model_id:
+        with open(latest_file, 'rb') as f:
+            return pickle.load(f)
+    else:
+        return None
+
+
 def main():
     # Parse input parameters
     parsed_args, unknown_args = parser.parse_known_args()
     parsed_args.num_steps = int(parsed_args.num_steps)
     unknown_args = parse_cmdline_kwargs(unknown_args)
+
+    # Create checkpoint path
+    parsed_args.ckpt_path = Path(parsed_args.ckpt_path)
+    i = 0
+    while parsed_args.ckpt_path.exists():
+        parsed_args.ckpt_path = parsed_args.ckpt_path.parent / (parsed_args.ckpt_path.name + str(i))
+        i += 1
+    else:
+        parsed_args.ckpt_path.mkdir()
+
+    # Run weights subscriber
+    subscriber = Process(target=run_weights_subscriber, args=(parsed_args,))
+    subscriber.start()
 
     if parsed_args.num_replicas > 1:
         agents = []
@@ -102,6 +149,8 @@ def main():
             agent.join()
     else:
         run_one_agent(0, parsed_args, unknown_args)
+
+    subscriber.join()
 
 
 if __name__ == '__main__':
--- learner.py
+++ learner.py
@@ -1,5 +1,6 @@
 import pickle
 from argparse import ArgumentParser
+from collections import deque
 from itertools import count
 
 import horovod.tensorflow.keras as hvd
@@ -25,8 +26,11 @@ parser = ArgumentParser()
 parser.add_argument('--alg', type=str, help='The RL algorithm', required=True)
 parser.add_argument('--env', type=str, help='The game environment', required=True)
 parser.add_argument('--num_steps', type=float, help='The number of training steps', required=True)
-parser.add_argument('--port', type=int, default=5000, help='Learner server port')
+parser.add_argument('--data_port', type=int, default=5000, help='Learner server port to receive training data')
+parser.add_argument('--param_port', type=int, default=5001, help='Learner server to publish model parameters')
 parser.add_argument('--model', type=str, default=None, help='Training model')
+parser.add_argument('--buffer_maxlen', type=int, default=100, help='The max length of training data buffer')
+parser.add_argument('--training_freq', type=int, default=100, help='How many steps are between each training')
 
 
 def main():
@@ -37,24 +41,44 @@ def main():
 
     # Expose socket to actor(s)
     context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind(f'tcp://*:{args.port}')
+    data_socket = context.socket(zmq.REP)
+    data_socket.bind(f'tcp://*:{args.data_port}')
+    weights_socket = context.socket(zmq.PUB)
+    weights_socket.bind(f'tcp://*:{args.param_port}')
 
     env, agent = init_components(args, unknown_args)
+
+    training_buffer = {
+        'states': deque(maxlen=args.buffer_maxlen),
+        'actions': deque(maxlen=args.buffer_maxlen),
+        'action_probs': deque(maxlen=args.buffer_maxlen),
+        'rewards': deque(maxlen=args.buffer_maxlen),
+        'next_states': deque(maxlen=args.buffer_maxlen),
+        'dones': deque(maxlen=args.buffer_maxlen)
+    }
 
     for step in count(1):
         # Do some updates
         agent.update_training(step, args.num_steps)
 
         # Receive data
-        data = parse_data(socket.recv())
+        data = parse_data(data_socket.recv())
+        data_socket.send(b'200')
+        training_buffer['states'].append(data[0])
+        training_buffer['actions'].append(data[1])
+        training_buffer['action_probs'].append(data[2])
+        training_buffer['rewards'].append(data[3])
+        training_buffer['next_states'].append(data[4])
+        training_buffer['dones'].append(data[5])
 
-        # Training
-        agent.learn(*data, step)
+        if step % args.training_freq == 0:
+            # Training
+            agent.learn(training_buffer['states'], training_buffer['actions'], training_buffer['action_probs'],
+                        training_buffer['rewards'], training_buffer['next_states'], training_buffer['dones'], step)
 
-        # Sync weights to actor
-        if hvd.rank() == 0:
-            socket.send(pickle.dumps(agent.get_weights()))
+            # Sync weights to actor
+            if hvd.rank() == 0:
+                weights_socket.send(pickle.dumps(agent.get_weights()))
 
 
 if __name__ == '__main__':
