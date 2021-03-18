b48338af15e0397b9a51cfee0fd89f150a537399
lixingze <lixingze@ustc.edu.cn>
2021-03-18 16:33:18 +0800

    Using placeholder for model.set_weight

--- models/tf_v1_model.py
+++ models/tf_v1_model.py
@@ -20,13 +20,23 @@ class TFV1Model(Model, ABC):
 
         super(TFV1Model, self).__init__(observation_space, action_space, model_id, config, *args, **kwargs)
 
+        self.build_assign()
         # Build saver
         self.saver = tf.train.Saver(tf.trainable_variables())
 
+    def build_assign(self):
+        self._weight_ph, self._to_assign = dict(), dict()
+        variables = tf.trainable_variables(self.scope)
+        for var in variables:
+            self._weight_ph[var.name] = tf.placeholder(var.value().dtype, var.get_shape().as_list())
+            self._to_assign[var.name] = var.assign(self._weight_ph[var.name])
+        self._nodes = list(self._to_assign.values())
+
     def set_weights(self, weights, *args, **kwargs) -> None:
-        self.sess.run(
-            [tf.assign(var, weight) for var, weight in zip(tf.trainable_variables(scope=self.scope), weights)]
-        )
+        feed_dict = {self._weight_ph[var.name]: weight
+                     for (var, weight) in zip(tf.trainable_variables(scope=self.scope), weights)}
+
+        self.sess.run(self._nodes, feed_dict=feed_dict)
 
     def get_weights(self, *args, **kwargs) -> Any:
         return self.sess.run(tf.trainable_variables(self.scope))
