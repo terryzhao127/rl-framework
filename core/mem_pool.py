f12e701ef9f3e0839be0a16712304f4b8fef5f1b
Guikarist <guikarist@gmail.com>
2021-04-23 10:07:51 +0800

    Calculate throughput statistics in 'MultiprocessingMemPool'

--- core/mem_pool.py
+++ core/mem_pool.py
@@ -1,11 +1,14 @@
+from __future__ import annotations
+
 import random
+import time
 from collections import defaultdict, deque
 from multiprocessing.managers import BaseManager
 from typing import Dict, List
 
 import numpy as np
 
-__all__ = ['MemPool', 'MemPoolManager']
+__all__ = ['MemPool', 'MultiprocessingMemPool', 'MemPoolManager']
 
 
 class MemPool:
@@ -52,8 +55,64 @@ class MemPool:
         return len(self.data[self._keys[0]])
 
 
+class MultiprocessingMemPool(MemPool):
+    def __init__(self, capacity: int = None, keys: List[str] = None) -> None:
+        super().__init__(capacity, keys)
+
+        self._receiving_data_throughput = None
+        self._consuming_data_throughput = None
+
+    def push(self, data: Dict[str, np.ndarray]) -> None:
+        super().push(data)
+
+        if self._receiving_data_throughput is not None:
+            self._receiving_data_throughput += len(data[self._keys[0]])
+
+    def sample(self, size: int = -1) -> Dict[str, np.ndarray]:
+        data = super().sample(size)
+
+        if self._consuming_data_throughput is not None:
+            self._consuming_data_throughput += len(data[self._keys[0]])
+
+        return data
+
+    def clear(self) -> None:
+        super().clear()
+
+        self._receiving_data_throughput = None
+        self._consuming_data_throughput = None
+
+    def _get_receiving_data_throughput(self):
+        return self._receiving_data_throughput
+
+    def _get_consuming_data_throughput(self):
+        return self._consuming_data_throughput
+
+    def _reset_receiving_data_throughput(self):
+        self._receiving_data_throughput = 0
+
+    def _reset_consuming_data_throughput(self):
+        self._consuming_data_throughput = 0
+
+    @classmethod
+    def print_throughput_fps(cls, obj: MultiprocessingMemPool, interval=10):
+        """Print receiving and consuming periodically"""
+
+        while True:
+            obj._reset_receiving_data_throughput()
+            obj._reset_consuming_data_throughput()
+
+            time.sleep(interval)
+
+            print(f'Receiving FPS: {obj._get_receiving_data_throughput() / interval:.2f}, '
+                  f'Consuming FPS: {obj._get_consuming_data_throughput() / interval:.2f}')
+
+
 class MemPoolManager(BaseManager):
     pass
 
 
-MemPoolManager.register('MemPool', MemPool, exposed=['__len__', 'push', 'sample', 'clear'])
+MemPoolManager.register('MemPool', MultiprocessingMemPool,
+                        exposed=['__len__', 'push', 'sample', 'clear', '_get_receiving_data_throughput',
+                                 '_get_consuming_data_throughput', '_reset_receiving_data_throughput',
+                                 '_reset_consuming_data_throughput'])
--- learner.py
+++ learner.py
@@ -1,7 +1,5 @@
-import multiprocessing
 import pickle
 import subprocess
-import time
 from argparse import ArgumentParser
 from itertools import count
 from multiprocessing import Process
@@ -13,7 +11,7 @@ from pyarrow import deserialize
 from tensorflow.keras.backend import set_session
 
 from common import init_components, load_yaml_config, save_yaml_config, create_experiment_dir
-from core.mem_pool import MemPoolManager
+from core.mem_pool import MemPoolManager, MultiprocessingMemPool
 from utils.cmdline import parse_cmdline_kwargs
 
 # Horovod: initialize Horovod.
@@ -34,7 +32,6 @@ parser.add_argument('--data_port', type=int, default=5000, help='Learner server 
 parser.add_argument('--param_port', type=int, default=5001, help='Learner server to publish model parameters')
 parser.add_argument('--model', type=str, default='acmlp', help='Training model')
 parser.add_argument('--pool_size', type=int, default=4000, help='The max length of data pool')
-parser.add_argument('--training_freq', type=int, default=1, help='How many steps are between each training')
 parser.add_argument('--batch_size', type=int, default=4000, help='The batch size for training')
 parser.add_argument('--exp_path', type=str, default=None, help='Directory to save logging data and config file')
 parser.add_argument('--config', type=str, default=None, help='The YAML configuration file')
@@ -64,39 +61,29 @@ def main():
     with open(args.exp_path / 'hash', 'w') as f:
         f.write(str(subprocess.run('git rev-parse HEAD'.split(), stdout=subprocess.PIPE).stdout.decode('utf-8')))
 
-    # Throughput-related data
-    time_start = multiprocessing.Value('d', time.time())
-    last_receiving_time = multiprocessing.Value('d', 0)
-    last_consuming_time = multiprocessing.Value('d', 0)
-    num_receiving_data = multiprocessing.Value('i', 0)
-    num_consuming_data = multiprocessing.Value('i', 0)
-
     # Start memory pool in another process
     manager = MemPoolManager()
     manager.start()
     mem_pool = manager.MemPool(capacity=args.pool_size)
-    Process(target=recv_data, args=(args.data_port, mem_pool, last_receiving_time, num_receiving_data)).start()
+    Process(target=recv_data, args=(args.data_port, mem_pool)).start()
 
     # Print throughput statistics
-    Process(target=print_throughput_fps,
-            args=(last_receiving_time, last_consuming_time, num_receiving_data, num_consuming_data, time_start)).start()
+    Process(target=MultiprocessingMemPool.print_throughput_fps, args=(mem_pool,)).start()
 
     for step in count(1):
         # Do some updates
         agent.update_training(step, args.num_steps)
 
-        if step % args.training_freq == 0 and len(mem_pool) >= args.batch_size:
+        if len(mem_pool) >= args.batch_size:
             # Training
             agent.learn(mem_pool.sample(size=args.batch_size))
-            num_consuming_data.value += args.batch_size
-            last_consuming_time.value = time.time()
 
             # Sync weights to actor
             if hvd.rank() == 0:
                 weights_socket.send(pickle.dumps(agent.get_weights()))
 
 
-def recv_data(data_port, mem_pool, last_receiving_time, num_receiving_data):
+def recv_data(data_port, mem_pool):
     context = zmq.Context()
     data_socket = context.socket(zmq.REP)
     data_socket.bind(f'tcp://*:{data_port}')
@@ -106,20 +93,6 @@ def recv_data(data_port, mem_pool, last_receiving_time, num_receiving_data):
         data: dict = deserialize(data_socket.recv())
         data_socket.send(b'200')
         mem_pool.push(data)
-
-        num_receiving_data.value += len(data[list(data.keys())[0]])
-        last_receiving_time.value = time.time()
-
-
-def print_throughput_fps(last_receiving_time, last_consuming_time, num_receiving_data, num_consuming_data, time_start,
-                         interval=10):
-    """Print receiving and consuming periodically"""
-    while True:
-        time.sleep(interval)
-
-        if last_receiving_time.value != 0 and last_consuming_time.value != 0:
-            print(f'Receiving FPS: {num_receiving_data.value / (last_receiving_time.value - time_start.value):.2f}, '
-                  f'Consuming FPS: {num_consuming_data.value / (last_consuming_time.value - time_start.value):.2f}')
 
 
 if __name__ == '__main__':
